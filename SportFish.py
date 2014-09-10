9aa5183991c0345ad710ff7f3ab766bdb21ade45
Jerry Yuan <yhyuan@gmail.com>
2014-09-10 16:09:46 -0400

    Add shape file output

--- SportFish.py
+++ SportFish.py
@@ -316,7 +316,7 @@ arcpy.CalculateField_management("station_Layer", "station.LOCNAME_FR", "[Station
 arcpy.CalculateField_management("station_Layer", "station.GUIDELOC_EN", "[StationOtherInfo.txt.GUIDELOC_EN]", "VB", "")
 arcpy.CalculateField_management("station_Layer", "station.GUIDELOC_FR", "[StationOtherInfo.txt.GUIDELOC_FR]", "VB", "")
 arcpy.RemoveJoin_management("station_Layer", "")
-
+arcpy.FeatureClassToShapefile_conversion(["station"], OUTPUT_PATH + "\\Shapefile")
 arcpy.Project_management(arcpy.env.workspace + "\\station", arcpy.env.workspace + "\\GuideLocs", "PROJCS['WGS_1984_Web_Mercator_Auxiliary_Sphere',GEOGCS['GCS_WGS_1984',DATUM['D_WGS_1984',SPHEROID['WGS_1984',6378137.0,298.257223563]],PRIMEM['Greenwich',0.0],UNIT['Degree',0.0174532925199433]],PROJECTION['Mercator_Auxiliary_Sphere'],PARAMETER['False_Easting',0.0],PARAMETER['False_Northing',0.0],PARAMETER['Central_Meridian',0.0],PARAMETER['Standard_Parallel_1',0.0],PARAMETER['Auxiliary_Sphere_Type',0.0],UNIT['Meter',1.0]]", "NAD_1983_To_WGS_1984_5", "GEOGCS['GCS_North_American_1983',DATUM['D_North_American_1983',SPHEROID['GRS_1980',6378137.0,298.257222101]],PRIMEM['Greenwich',0.0],UNIT['Degree',0.0174532925199433]]")
 arcpy.Delete_management(arcpy.env.workspace + "\\station", "FeatureClass")
 
